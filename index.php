0c812e9d1604c8108c807e9a9249f540321ad56c
DimVechorko <ganerals@gmail.com>
2014-07-08 12:12:51 +0300

    oop
    oop

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 define('DB_HOST', 'localhost');
 
 session_start();
-require_once ('form.html');
+
 require_once ('classes.php');
 
 $dbc = new ConnectMysqlDB();
-$pagination = new Pagination();
-$postlist= new Postlist();
+
 $validation = new Validation();
 $validation_email = new EmailAddressValidator();
 
-
-
-
-
-$pagination=$pagination->generate_page_links($num_pages);
-echo $pagination;
-
-$postlist=$postlist->outputMessages();
-
 $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS,'guestbook');
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
 if (isset($_POST['submit'])) {
     $user_name = strip_tags(mysqli_real_escape_string($dbc,trim($_POST['name'])));
@@ -33,13 +27,13 @@ if (isset($_POST['submit'])) {
 
 
     $valid_name = $validation->validName($user_name);
-    var_dump($valid_name);
+    //var_dump($valid_name);
     $valid_comment = $validation->validComment($comment);
-    var_dump($valid_comment);
+    //var_dump($valid_comment);
     $valid_captcha = $validation->validCaptcha($captcha);
-    var_dump($valid_captcha);
+    //var_dump($valid_captcha);
     $valid_email = $validation_email ->check_email_address($strEmailAddress);
-    var_dump($valid_email);
+    //var_dump($valid_email);
     if ($valid_email == true) {
         // Оставленный здесь return, т.к. он вне фукнции останавливает выполнение скрипта
         // И до сохранения сообщения не доходит
@@ -52,15 +46,25 @@ if (isset($_POST['submit'])) {
     if ($valid_name==1 && $valid_comment==true && $valid_captcha==true && $valid_email==true) {
         //echo $user_name; echo $strEmailAddress; echo $comment;
         $query="INSERT INTO guests (username, email, comment, date_posted)VALUE ('$user_name','$strEmailAddress','$comment',NOW())";
-        $result=mysqli_query($dbc,$query)or die("error query");
+        var_dump($query);
+        $result=mysqli_query($dbc,$query)or die("error query:" . mysqli_error($dbc));
         // Отключаем echo, для нормальной работы редиректа
         //echo '<p>' . $user_name .  ', Сообщение отправлено!</p>';
-        header( 'Location: index.php');
+        //header( 'Location: index.php');
+        //exit();
     }
 }
 
+// Этот блок не нужно обрабатывать при корректном сабмите формы, так что его лучше поместить здесь
+// чтобы зря не выполнять
+// кроме того он будет мешать корректной работе header ghb сабмите формы, если будет находиться выше
+require_once ('form.html');
+$pagination = new Pagination();
+$postlist= new Postlist();
+$pagination=$pagination->generate_page_links($num_pages);
+echo $pagination;
+$postlist=$postlist->outputMessages();
 
-
-
-session_destroy();
+// Какое назначение этой строки?
+// session_destroy();
 
